7cb570002fd3ed06a8b1af6fa6153779f4a359c9
Matheus-FSantos <matheus.fs.contato@gmail.com>
2023-03-24 10:42:56 -0300

    Projeto - Segunda Implementação, finalização dos endpoints e CRUD

---
+++ Versão em Spring/Segunda Implementação/Estudantes-API/src/main/java/com/api/estudantes/EstudantesApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.estudantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudantesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudantesApiApplication.class, args);
+	}
+
+}
---
+++ Versão em Spring/Segunda Implementação/Estudantes-API/src/test/java/com/api/estudantes/EstudantesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.estudantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudantesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
